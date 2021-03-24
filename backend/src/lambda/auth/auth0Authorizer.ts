d68eaa8c3934b5fee4a8da5f8fc84b5a6831ca5b
Chomnoue Nghemning Alain-Michel <chomnoue@yahoo.fr>
2021-03-24 09:26:05 +0800

    implement auth0 authorizer

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,19 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import {CustomAuthorizerEvent, CustomAuthorizerResult} from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
+import {decode, verify} from 'jsonwebtoken'
+import {createLogger} from '../../utils/logger'
+import {Jwt} from '../../auth/Jwt'
+import {JwtPayload} from '../../auth/JwtPayload'
+import * as JwksRsa from 'jwks-rsa'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = process.env.JWKS_URL
+const jwksClient = JwksRsa({jwksUri: jwksUrl})
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +62,9 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const key = await jwksClient.getSigningKey(jwt.header.kid);
+  const signingKey = key.getPublicKey();
+  return verify(token, signingKey, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -71,7 +74,5 @@ function getToken(authHeader: string): string {
     throw new Error('Invalid authentication header')
 
   const split = authHeader.split(' ')
-  const token = split[1]
-
-  return token
+  return split[1]
 }
--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -6,7 +6,7 @@ import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
-
+  // TODO: first implement a user extraction middleware
   // TODO: Implement creating a new TODO item
   return undefined
 }
